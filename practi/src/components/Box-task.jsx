e60c06c225ebdb2fbbce7a9ee9b5d84c568a15ff
K3ndryRSoto <kendrisoto@gmail.com>
2023-06-16 15:03:12 -0400

    feat/add-funtiones-app

--- practi/src/components/Box-task.jsx
+++ practi/src/components/Box-task.jsx
@@ -1,23 +1,32 @@
 import { useState } from "react";
-import Swal from "sweetalert2";
-import { FaPen, FaTrash } from "react-icons/fa";
+import { FaTrash, FaPen } from "react-icons/fa";
+
 export function Tasks({ item }) {
   const [isEdit, setIsEdit] = useState(false);
-  return (
-    <div className="box-task">
-      {item.title}
-      <FaPen
-        onClick={() => {
-          Swal.fire("This can't be edited yet");
-          className = "icon";
-        }}
-      />
-      <FaTrash
-        onClick={() => {
-          Swal.fire("This still can't be removed");
-          className = "icon";
-        }}
-      />
-    </div>
-  );
+
+  function Form() {
+    return (
+      <form>
+        <input type="text" />
+        <button>Update</button>
+      </form>
+    );
+  }
+
+  function TaskInfo() {
+    return (
+      <div className="box-task">
+        {item.title}
+        <FaPen
+          onClick={() => {
+            setIsEdit(true);
+          }}
+        />
+
+        <FaTrash />
+      </div>
+    );
+  }
+
+  return <>{isEdit ? <Form /> : <TaskInfo />}</>;
 }
