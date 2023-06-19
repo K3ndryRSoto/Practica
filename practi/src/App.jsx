8eb464c076df638757370d070f92efba9a64d110
Kendry R. Soto <113248017+KendryRSoto@users.noreply.github.com>
2023-06-19 15:31:50 -0400

    feat: add funtion app (#2)

--- practi/src/App.jsx
+++ practi/src/App.jsx
@@ -1,5 +1,5 @@
-import Swal from "sweetalert2/dist/sweetalert2.js";
 import { useState } from "react";
+import Swal from "sweetalert2";
 import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 import { Tasks } from "./components/Box-task";
@@ -18,16 +18,44 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (!info) {
+      return Swal.fire("The text box is empty");
+    }
 
     const newTask = {
       id: uuid,
       title: info,
       complete: false,
     };
-
     const pre = [...task];
     pre.unshift(newTask);
     setTask(pre);
+    setInfo("");
+  };
+
+  const handleUpdate = (id, value) => {
+    const pre = [...task];
+    const item = pre.find((item) => item.id === id);
+    item.title = value;
+    setTask(pre);
+  };
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const pre = task.filter((item) => item.id !== id);
+        setTask(pre);
+        Swal.fire("Deleted!", "Your file has been deleted.", "success");
+      }
+    });
   };
 
   return (
@@ -36,6 +64,7 @@ function App() {
         <input
           name="title"
           type="text"
+          value={info}
           className="input-text"
           onChange={handleOnchange}
         />
@@ -45,11 +74,16 @@ function App() {
 
       <div>
         {task.map((item) => (
-          <Tasks key={item.id} item={item} />
+          <Tasks
+            key={item.id}
+            item={item}
+            task={task}
+            update={handleUpdate}
+            deleteTask={handleDelete}
+          />
         ))}
       </div>
     </>
-    // hi
   );
 }
 export default App;
--- practi/src/components/Box-task.jsx
+++ practi/src/components/Box-task.jsx
@@ -1,12 +1,49 @@
 import { useState } from "react";
-import { FaPen, FaTrash } from "react-icons/fa";
-export function Tasks({ item }) {
+import { FaTrash, FaPen, FaCheck } from "react-icons/fa";
+
+export function Tasks({ item, update, deleteTask }) {
   const [isEdit, setIsEdit] = useState(false);
-  return (
-    <div>
-      {item.title}
-      <FaPen />
-      <FaTrash />
-    </div>
-  );
+
+  const Form = () => {
+    const [newValue, setNewValue] = useState(item.title);
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+    };
+
+    const handleChange = (e) => {
+      const value = e.target.value;
+      setNewValue(value);
+    };
+
+    const handleClicUpdate = () => {
+      update(item.id, newValue);
+      setIsEdit(false);
+    };
+
+    return (
+      <form className="box-form" onSubmit={handleSubmit}>
+        <input type="text" onChange={handleChange} value={newValue} />
+
+        <FaCheck className="button-update" onClick={handleClicUpdate} />
+      </form>
+    );
+  };
+
+  const TaskInfo = () => {
+    return (
+      <div className="box-task">
+        {item.title}
+        <FaPen
+          onClick={() => {
+            setIsEdit(true);
+          }}
+        />
+
+        <FaTrash onClick={(e) => deleteTask(item.id)} />
+      </div>
+    );
+  };
+
+  return <>{isEdit ? <Form /> : <TaskInfo />}</>;
 }
